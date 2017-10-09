50adaf1ff87cc160a91368cd0a98234a6b1e1aff
Victor Sergienko <singalen@gmail.com>
2017-10-08 21:21:33 -0700

    iOS specific settings: * #6 Hide and forever enable «pause music on focus loss» - stop_music_in_background never needs to be false on mobiles; so default it to true and hide it.

--- src/preferences/general.cpp
+++ src/preferences/general.cpp
@@ -706,7 +706,7 @@ bool set_music(bool ison) {
 
 bool stop_music_in_background()
 {
-	return get("stop_music_in_background", false);
+	return get("stop_music_in_background", true);
 }
 
 void set_stop_music_in_background(bool ison)
