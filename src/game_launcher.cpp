3c3c8ae86652886df428869abfeb83b0a2278fd7
Victor Sergienko <singalen@gmail.com>
2017-10-09 22:31:59 -0700

    iOS: No wesnothd, no system().

--- src/game_launcher.cpp
+++ src/game_launcher.cpp
@@ -815,7 +815,8 @@ bool game_launcher::goto_editor()
 
 void game_launcher::start_wesnothd()
 {
-	const std::string wesnothd_program =
+#ifndef __IPHONEOS__
+    const std::string wesnothd_program =
 		preferences::get_mp_server_program_name().empty() ?
 		filesystem::get_program_invocation("wesnothd") : preferences::get_mp_server_program_name();
 
@@ -843,6 +844,7 @@ void game_launcher::start_wesnothd()
 		return;
 	}
 	preferences::set_mp_server_program_name("");
+#endif
 
 	// Couldn't start server so throw error
 	WRN_GENERAL << "Failed to run server start script" << std::endl;
