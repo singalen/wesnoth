37db4d5ce0def831b1bf994cf405f4740807c136
Victor Sergienko <singalen@gmail.com>
2017-12-22 11:37:58 -0800

    iOS: Touch events handled in the first approach, and a RMB emulation of such.

--- src/config_cache.cpp
+++ src/config_cache.cpp
@@ -27,6 +27,7 @@
 
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
+#include <SDL_platform.h>
 
 static lg::log_domain log_cache("cache");
 #define ERR_CACHE LOG_STREAM(err, log_cache)
@@ -76,6 +77,10 @@ void config_cache::clear_defines()
 
 #ifdef __APPLE__
 	defines_map_["APPLE"] = preproc_define();
+#endif
+
+#if defined(MOUSE_TOUCH_EMULATION) || defined(__IPHONEOS__)
+	defines_map_["IPHONEOS"] = preproc_define();
 #endif
 
 	defines_map_["WESNOTH_VERSION"] = preproc_define(game_config::wesnoth_version.str());
