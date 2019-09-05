c7742c2bd9b5e58a00f5800aa5cc94ca62a705ea
Victor Sergienko <singalen@gmail.com>
2019-09-04 23:17:48 -0700

    iOS: WIP: functions for iCloud upload.

---
+++ src/ios/ios_filesystem.h
@@ -0,0 +1,29 @@
+/*
+ Copyright (C) 2019 by Victor Sergienko <singalen@g.com>
+ Part of the Battle for Wesnoth Project https://www.wesnoth.org/
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY.
+ 
+ See the COPYING file for more details.
+ */
+
+#ifndef ios_filesystem_h
+#define ios_filesystem_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *Wesnoth_GetDocumentsPath(const char *org, const char *app);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* ios_filesystem_h */
